ce8bd34abd2875e6b4aa2ca344d84f96263369e0
Niayesh <niya.zargar@gmail.com>
2025-07-04 14:09:41 +0330

    Function add customer from options and list customers created

--- modules/Customer.py
+++ modules/Customer.py
@@ -8,3 +8,22 @@ class Customer:
     def __str__(self):
         return f"Name: {self.name}, Father name: {self.father_name}, Customer ID: {self.customer_id}, Balance: {self.balance}"
     
+
+list_customers = []    
+
+def add_customer():
+    while True :
+        customer_id = input("Enter your Customer ID: ")
+        if any(customer.customer_id == customer_id for customer in list_customers):
+            print("This ID is already exists.Please enter a uinque one.")
+        else : 
+            new_name = input("Enter your name: ")
+            new_father_name = input("Enter your father's name: ")
+            try:
+                balance = float(input("Enter you initial balance without any marks like $: "))
+            except ValueError :
+                print("Invalid balance input.Try again.")
+                return
+            
+        new_customer = Customer(new_name, new_father_name, customer_id, balance)
+        list_customers.append(new_customer)
