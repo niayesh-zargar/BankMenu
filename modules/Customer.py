9069974baf25910e7a1ffd462bde1ca36e490ef5
Niayesh <niya.zargar@gmail.com>
2025-07-04 13:53:42 +0330

    Function init from Class Customer created

---
+++ modules/Customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self, name, father_name, customer_id, balance):
+        self.name = name
+        self.father_name = father_name
+        self.customer_id = customer_id
+        self.balance = balance
+        
+    
