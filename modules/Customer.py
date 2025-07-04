333aa9cf586f54e533651116b39a3771abd9d401
Niayesh <niya.zargar@gmail.com>
2025-07-04 14:32:36 +0330

    Functions edit and delete customer and request loan from options created

--- modules/Customer.py
+++ modules/Customer.py
@@ -20,7 +20,7 @@ def add_customer():
             new_name = input("Enter your name: ")
             new_father_name = input("Enter your father's name: ")
             try:
-                balance = float(input("Enter you initial balance without any marks like $: "))
+                balance = float(input("Enter your initial balance without any marks like $: "))
             except ValueError :
                 print("Invalid balance input.Try again.")
                 return
@@ -45,3 +45,50 @@ def show_customers():
             print("List Customers: ")
             print(customer)
             
+def edit_customer():
+    customer_id = input("Enter yor ID: ")
+    for customer in list_customers:
+        if customer.customer_id == customer_id :
+            print("Customer found: ")
+            new_name = input("Enter your new name: ")
+            customer.name = new_name
+            print("Name updated successfully.")
+            try:
+                new_balance = float(input("Enter your new initial balance without any marks like $: "))
+            except ValueError :
+                print("Invalid balance input.Try again.")
+                return
+            customer.balance = new_balance
+            print("Initial balance updated successfully.")
+            return
+    print("Customer not found.")
+    
+def delete_customer():
+        customer_id = input("Enter yor ID: ")
+        for customer in list_customers:  
+            if customer.customer_id == customer_id :
+                list_customers.remove(customer)
+                print("Customer deleted.")
+            return
+        print("Customer not found.") 
+
+def request_loan():
+        customer_id = input("Enter yor ID: ")
+        for customer in list_customers:  
+            if customer.customer_id == customer_id :
+                try:
+                    amount = float(input("Enter the amount of loan without any marks like $: "))
+                except ValueError :
+                    print("Invalid amount.")
+                    return
+                
+                if amount <= customer.balance :
+                    customer.balance += amount
+                    print(f"Loan approved! New balance: {customer.balance}")
+                else : 
+                    print("Loan amount exceeds balance.")
+                    return
+        print("Customer not found.")
+                    
+                    
+        
